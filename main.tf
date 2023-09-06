19496b0cdf64b3fce621a873bafa0ef1af121d8e
rej23 <rejamada@gmail.com>
2023-09-06 15:41:28 +0100

    "added gitignore"

--- main.tf
+++ main.tf
@@ -10,8 +10,8 @@ terraform {
 
 provider "aws" {
     region = "us-east-1"
-    #access_key = ""
-    #secret_key = ""
+    access_key = var.access_key
+    secret_key = var.secret_key
     #### alternative for using aws configure #####
 }
 
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "access_key" {
+    description = "Access Key"
+    type        = string
+    sensitive   = true
+
+}
+
+ 
+
+variable "secret_key" {
+    description = "Secret Key"
+    type        = string
+    sensitive   = true
+
+}
